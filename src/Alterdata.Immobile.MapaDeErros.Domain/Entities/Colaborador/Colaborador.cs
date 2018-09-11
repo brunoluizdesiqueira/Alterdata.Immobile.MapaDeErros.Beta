d4e9ff493ea1d44421f85efe4cd719d92411a3af
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2018-09-10 21:50:47 -0300

    Commit inicial

---
+++ src/Alterdata.Immobile.MapaDeErros.Domain/Entities/Colaborador/Colaborador.cs
@@ -0,0 +1,12 @@
+namespace Alterdata.Immobile.MapaDeErros.Domain.Entities.Colaborador
+{
+    using System;
+    public class Colaborador
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; } 
+
+        public DateTime? DataDesativacao { get; set; }
+    }
+}
---
+++ src/Alterdata.Immobile.MapaDeErros.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Alterdata.Immobile.MapaDeErros.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
